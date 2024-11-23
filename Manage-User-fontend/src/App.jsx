cd2fa2c57ab63656c2e2fa8e6b7ae25058a9a994
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-23 22:15:29 +0600

    Add a New Data

--- Manage-User-fontend/src/App.jsx
+++ Manage-User-fontend/src/App.jsx
@@ -2,11 +2,13 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+///==================================
+
 const url = "http://localhost:5000/users";
 
 function App() {
 
-  const [user, setUser] = useState([]);
+  const [User, setUser] = useState([]);
 
 
   useEffect(() => {
@@ -20,13 +22,53 @@ function App() {
 
 
 
+  const AddingUser = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    if (name == "" && email == "") {
+      alert("fill up all field")
+      return;
+    }
+    const user = {
+      name, email
+    }
+    fetch("http://localhost:5000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    }).then((res) => res.json()).then((data) => {
+      form.reset();
+      console.log(data.user);
+      const newUSer = [...User, data];
+      setUser(newUSer);
+
+    })
+
+  }
+
+
+
 
   return (
-    <>
+    <div>
       <h1 className='text-2xl '> User management Fontend</h1>
-      <p>
+      <div >
+        <form onSubmit={AddingUser} className='grid grid-cols-1 container mx-auto p-4 border w-[300px] gap-2 rounded-lg'>
+          <input className='p-2 border rounded-md' type="text" name='name' placeholder='Enter user name' />
+          <input className='p-2 border rounded-md' type="text" name='email' placeholder='Enter Your Email' />
+          <button className='border rounded text-xl p-2' type='submit'>Add User</button>
+        </form>
+        <div className='m-4 w-[300px] h-10'>
+        </div>
+      </div>
+      <div>
         {
-          user.map((user, index) => {
+          User.map((user, index) => {
             return (
               <div key={index} className='flex border p-4 shadow-lg gap-2 m-4  rounded-lg'>
                 <h2>{user.id}</h2>
@@ -36,9 +78,10 @@ function App() {
             )
           })
         }
-      </p>
+      </div>
 
-    </>
+
+    </div>
   )
 }
 
--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+const port = process.env.PORT || 5000;
+//=====================
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.json());
 
-const User = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
-
-]
+let User = [];
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+
 app.get("/users", (req, res) => {
-    res.send(data = User);
-})
+    res.send(User); // Send the User array
+});
+
+
+app.post('/users', (req, res) => {
+    console.log("Post API Hitting");
+    const newUser = req.body;
+    newUser.id = User.length + 1; // Add ID
+    User.push(newUser); // Add to User array
+    res.status(201).send({ message: 'User added successfully', user: newUser });
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
