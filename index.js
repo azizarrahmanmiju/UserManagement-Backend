e0bce6372b9647cec6bbe55f067cc2b0d972bef1
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-23 17:37:09 +0600

    User respose send

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+
+const User = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+
+]
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+app.get("/users", (req, res) => {
+    res.send(User);
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-}) 
+    console.log(`Server is running on port ${port}`);
+});
